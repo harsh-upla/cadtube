8e9ae2f627f5c5709a4ef1fd6f1af41a956f3aac
henry-150 <dsharsh150@gmail.com>
2026-06-05 19:01:41 +0530

    meta and script fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,10 @@ import Script from "next/script";
 export const metadata: Metadata = {
   title: "CadTube",
   description: "Download videos from YouTube.",
+  other: {
+    "google-adsense-account": "ca-pub-9342853720075226",
+    "monetag": "5eca832e5b87b09ba6dc00bbb6c26d7e",
+  },
 };
 
 export default function RootLayout({
@@ -16,12 +20,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" >
-      <head>
-        <Script id="monetag-ad-tag" strategy="afterInteractive">{`(function(s){s.dataset.zone='11105050',s.src='https://al5sm.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`}</Script>
-        <meta name="google-adsense-account" content="ca-pub-9342853720075226"></meta>
-        <meta name="monetag" content="5eca832e5b87b09ba6dc00bbb6c26d7e"></meta>
-      </head>
       <body className="w-full h-full bg-[#e2e1dc]">
+        <Script 
+          src="https://al5sm.com/tag.min.js" 
+          data-zone="11105050" 
+          strategy="beforeInteractive" 
+        />
         <Navbar />
         {children}
       </body>
